1ee2cf1cae6c31f3de3d288c3ed7c30347ffaf9c
mzamarri <miguelazamarripar@gmail.com>
2024-06-14 22:56:56 -0700

    Adding css styling to Order History

--- app/static/views/OrderHistoryView.js
+++ app/static/views/OrderHistoryView.js
@@ -59,10 +59,19 @@ export default class extends AbstractView {
                             imageSrc: "#",
                             description: "Product 3 description"
                         }
+                    },
+                    "2": {
+                        amount: 2,
+                        item: {
+                            name: "Product 2",
+                            price: 25.00,
+                            imageSrc: "#",
+                            description: "Product 2 description"
+                        }
                     }
                 }
             }
-        ];;
+        ];
     }
 
     getHtml() {
@@ -71,27 +80,43 @@ export default class extends AbstractView {
         return `
             <div class="order-history-container">
                 <h1>Order History</h1>
-                <div class="order-history">
+                <div class="order-history" id="order-history">
                     ${orderHistory.map(order => `
                             <div class="order">
-                                <h2>Order ID: ${order.id}</h2>
-                                <h3>Order Date: ${order.date}</h3>
-                                <h3>Order Total: $${order.totalPrice}</h3>
+                                <div class="order-summary">
+                                    <h2>Order ID: ${order.id}</h2>
+                                    <h3>Order Date: ${order.date}</h3>
+                                    <h3>Order Total: $${order.totalPrice}</h3>
+                                </div>
                                 <div class="order-items">
                                     ${Object.entries(order.items).map(([id, orderItem]) => `
-                                        <div class="order-item" data-id="${id}">
+                                        <div class="item" data-id="${id}">
                                             <img src="${orderItem.item.imageSrc}" alt="product photo">
                                             <h3>${orderItem.item.name}</h3>
                                             <h3>$${orderItem.item.price}</h3>
                                             <h3>Quantity: ${orderItem.amount}</h3>
                                             <h3>Subtotal: $${orderItem.item.price * orderItem.amount}</h3>
                                         </div>
-                                    `)}
+                                    `).join(" ")}
                                 </div>
                             </div>
-                        `).join("")}
+                        `).join(" ")}
                 </div>
             </div>
         `
+    }
+
+    showItems(event) {
+
+    }
+
+    setEventListeners() {
+        const orders = document.getElementById("order-history").querySelectorAll(".order");
+        orders.forEach(order => {
+            order.querySelector(".order-summary").addEventListener("click", (e) => {
+                const orderItems = e.currentTarget.parentNode.querySelector(".order-items");
+                orderItems.classList.toggle("show-items");
+            });
+        });
     }
 }
---
+++ logging/CartLogger.js
@@ -0,0 +1,9 @@
+const AbstractLogger = require("./AbstractLogger.js");
+
+module.export = class extends AbstractLogger {
+    constuctor(isLogging) {
+        super(isLogging);
+    }
+
+    
+}
